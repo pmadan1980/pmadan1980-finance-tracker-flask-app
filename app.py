011fc2a568d4478d985653dac873a27cd9e0b812
Prarthana Madan <prarthana@Prarthanas-MacBook-Air-2.local>
2024-08-12 11:54:23 +0530

    Initial commit of finance-tracker-flask-app

--- app.py
+++ app.py
@@ -1,79 +1,68 @@
-# to-do list flask app
-
 import os
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 from datetime import date
 
 #### Defining Flask App
 app = Flask(__name__)
 
-
 #### Saving Date today in 2 different formats
 datetoday = date.today().strftime("%m_%d_%y")
 datetoday2 = date.today().strftime("%d-%B-%Y")
 
-
 #### If this file doesn't exist, create it
-if 'tasks.txt' not in os.listdir('.'):
-    with open('tasks.txt','w') as f:
+if 'expenses.txt' not in os.listdir('.'):
+    with open('expenses.txt', 'w') as f:
         f.write('')
 
+def get_expenselist():
+    with open('expenses.txt', 'r') as f:
+        expenselist = f.readlines()
+    return expenselist
 
-def gettasklist():
-    with open('tasks.txt','r') as f:
-        tasklist = f.readlines()
-    return tasklist
-
-def createnewtasklist():
-    os.remove('tasks.txt')
-    with open('tasks.txt','w') as f:
+def create_new_expenselist():
+    os.remove('expenses.txt')
+    with open('expenses.txt', 'w') as f:
         f.write('')
 
-def updatetasklist(tasklist):
-    os.remove('tasks.txt')
-    with open('tasks.txt','w') as f:
-        f.writelines(tasklist)
-
+def update_expenselist(expenselist):
+    os.remove('expenses.txt')
+    with open('expenses.txt', 'w') as f:
+        f.writelines(expenselist)
 
 ################## ROUTING FUNCTIONS #########################
 
 #### Our main page
 @app.route('/')
 def home():
-    return render_template('home.html',datetoday2=datetoday2,tasklist=gettasklist(),l=len(gettasklist())) 
+    return render_template('home.html', datetoday2=datetoday2, expenselist=get_expenselist(), l=len(get_expenselist()))
 
-
-# Function to clear the to-do list
+# Function to clear the expense list
 @app.route('/clear')
 def clear_list():
-    createnewtasklist()
-    return render_template('home.html',datetoday2=datetoday2,tasklist=gettasklist(),l=len(gettasklist())) 
+    create_new_expenselist()
+    return render_template('home.html', datetoday2=datetoday2, expenselist=get_expenselist(), l=len(get_expenselist()))
 
+# Function to add an expense to the list
+@app.route('/addexpense', methods=['POST'])
+def add_expense():
+    expense_description = request.form.get('expense_description')
+    expense_amount = request.form.get('expense_amount')
+    expense_entry = f"{expense_description} - ${expense_amount}\n"
+    with open('expenses.txt', 'a') as f:
+        f.writelines(expense_entry)
+    return render_template('home.html', datetoday2=datetoday2, expenselist=get_expenselist(), l=len(get_expenselist()))
 
-# Function to add a task to the to-do list
-@app.route('/addtask',methods=['POST'])
-def add_task():
-    task = request.form.get('newtask')
-    with open('tasks.txt','a') as f:
-        f.writelines(task+'\n')
-    return render_template('home.html',datetoday2=datetoday2,tasklist=gettasklist(),l=len(gettasklist())) 
-
-
-# Function to remove a task from the to-do list
-@app.route('/deltask',methods=['GET'])
-def remove_task():
-    task_index = int(request.args.get('deltaskid'))
-    tasklist = gettasklist()
-    print(task_index)
-    print(tasklist)
-    if task_index < 0 or task_index > len(tasklist):
-        return render_template('home.html',datetoday2=datetoday2,tasklist=tasklist,l=len(tasklist),mess='Invalid Index...') 
+# Function to remove an expense from the list
+@app.route('/delexpense', methods=['GET'])
+def remove_expense():
+    expense_index = int(request.args.get('delexpenseid'))
+    expenselist = get_expenselist()
+    if expense_index < 0 or expense_index >= len(expenselist):
+        return render_template('home.html', datetoday2=datetoday2, expenselist=expenselist, l=len(expenselist), mess='Invalid Index...')
     else:
-        removed_task = tasklist.pop(task_index)
-    updatetasklist(tasklist)
-    return render_template('home.html',datetoday2=datetoday2,tasklist=tasklist,l=len(tasklist)) 
-    
-
+        expenselist.pop(expense_index)
+    update_expenselist(expenselist)
+    return render_template('home.html', datetoday2=datetoday2, expenselist=expenselist, l=len(expenselist))
 
 #### Our main function which runs the Flask App
 if __name__ == '__main__':
