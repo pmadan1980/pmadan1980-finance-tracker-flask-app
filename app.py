0f74adcb232d61dd57ade8313ebe4970d234108d
pmadan1980 <149643151+pmadan1980@users.noreply.github.com>
2024-08-20 15:17:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+import matplotlib
+matplotlib.use('Agg')  # Use the Agg backend to avoid GUI issues on macOS
+
+from flask import Flask, render_template, request, redirect, url_for, flash, session, Response
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 's3cr3t!k3y#1234567890abcdef'  # Replace with a securely generated secret key
@@ -77,14 +83,25 @@ def home():
 
     if request.method == 'POST':
         description = request.form['expense_description']
-        amount = request.form['expense_amount']
+        amount = float(request.form['expense_amount'])
         category_id = request.form['expense_category']
         new_expense = Expense(description=description, amount=amount, user_id=user_id, category_id=category_id)
         db.session.add(new_expense)
         db.session.commit()
         return redirect(url_for('home'))
     
-    return render_template('home.html', expenselist=expenses, datetoday2='Today', categories=categories)
+    total_expenses = sum(expense.amount for expense in expenses)
+
+    # Generate the pie chart
+    category_names = [category.name for category in categories]
+    category_expenses = [
+        sum(expense.amount for expense in expenses if expense.category_id == category.id)
+        for category in categories
+    ]
+
+    pie_chart_url = generate_pie_chart(category_names, category_expenses)
+    
+    return render_template('home.html', expenselist=expenses, datetoday2='Today', categories=categories, total_expenses=total_expenses, pie_chart_url=pie_chart_url)
 
 @app.route('/delexpense')
 def delexpense():
@@ -130,8 +147,28 @@ def add_category():
     
     return redirect(url_for('home'))
 
+def generate_pie_chart(labels, values):
+    # Define your color palette
+    colors = ['#bb9af7', '#2ac3de', '#453a68', '#833c5c', '#322c52']
+
+    plt.figure(figsize=(6, 6))
+    plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140, colors=colors)
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.tight_layout()
+
+    # Save the pie chart to a bytes buffer
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    pie_chart_data = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    buffer.close()
+
+    # Return the base64-encoded image data as a string
+    return f"data:image/png;base64,{pie_chart_data}"
+
+
 # Initialize the database
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Create the database tables if they don't exist
+        db.create_all()
     app.run(debug=True)
